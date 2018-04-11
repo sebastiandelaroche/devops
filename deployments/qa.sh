37867ccbe020955d5ed1cd724137baaf2d907da6
sebastianroche <sebastian@softdev.team>
2018-04-10 20:40:28 -0500

    Change deployments config qa

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,6 +1,5 @@
-# sudo npm install now -g
-# echo "deploying..."
-# URL=$(now --docker -t $NOW_TOKEN)
-# echo "running acceptance on $URL"
-# curl --silent -L $URL
-echo $NOW_TOKEN
+sudo npm install now -g
+echo "deploying..."
+URL=$(now --docker -t $NOW_TOKEN)
+echo "running acceptance on $URL"
+curl --silent -L $URL
