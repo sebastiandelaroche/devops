2bc139925c091ac10ea56dd81b308de21fb10fb8
sebastianroche <sebastian@softdev.team>
2018-04-12 22:22:15 -0500

    Add Terraform Architecture

---
+++ infra/main.tf
@@ -0,0 +1,49 @@
+resource "digitalocean_loadbalancer" "devops" {
+  name   = "devops-lb2"
+  region = "nyc1"
+
+  forwarding_rule {
+    entry_port     = 80
+    entry_protocol = "http"
+
+    target_port     = 3000
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port     = 3000
+    protocol = "http"
+    path     = "/"
+  }
+
+  droplet_tag = "${digitalocean_tag.devops.name}"
+}
+
+resource "digitalocean_tag" "devops" {
+  name = "devops"
+}
+
+resource "digitalocean_droplet" "devops" {
+  count    = 2
+  image    = "33397121"
+  name     = "devops"
+  region   = "nyc1"
+  size     = "512mb"
+  ssh_keys = [19927153]
+  tags     = ["${digitalocean_tag.devops.id}"]
+
+  user_data = <<EOF
+#cloud-config
+coreos:
+    units:
+        - name: "devops.service"
+          command: "start"
+          content: |
+            [Unit]
+            Description=devops
+            After=docker.service
+
+            [Service]
+            ExecStart=/usr/bin/docker run -d -p 3000:3000 devops
+    EOF
+}
