0d97ba468f86b3faf149ae1a70706b94a7899a93
sebastianroche <sebastian@softdev.team>
2018-04-15 12:19:04 -0500

    Change message

--- src/config/app.js
+++ src/config/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hi world from devops professional with integration test from digital ocean!');
+    res.send('Hi world from digital ocean!');
 });
 
 module.exports = app;
